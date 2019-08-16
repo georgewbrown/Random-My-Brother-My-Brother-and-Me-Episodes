5d4adbac761e5bab5dac24f100758f74873bdbf5
georgewbrown <396gbrown@gmail.com>
2019-08-16 16:50:08 -0400

    [WIP] removed .dotenv temp until solution found

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+// require('dotenv').config();
+
 $(document).ready(function () {
-    const API_KEY = require('./env');
     
-    const key = API_KEY;
-    console.log(key);
+    // const key = process.env.API_KEY;
+    // console.log(key);
     const playlistId = 'PLt5zc-HxINkpJSBb_oeYVsSXgpxAlxQEc';
     const URL = 'https://www.googleapis.com/youtube/v3/playlistItems';
 
     const options = {
         part: 'snippet',
-        key: key,
+        // key: key,
         maxResults: 0,
         playlistId: playlistId
     }
@@ -20,6 +21,7 @@ $(document).ready(function () {
         })
 
         // console.log(totalEpisodes);
+        // document.querySelector('.ytp-title-link').innerText;
         // TO-DO: make numberOfEpisodes equal to the number of items in the playlist index
         const numberOfEpisodes = 371;
         const numberOfUnlisted = 38;
@@ -31,8 +33,10 @@ $(document).ready(function () {
         }
         
         getEpisodeBtn.addEventListener('click', function () {
-            videoContainer.innerHTML = `<iframe width="560" height="315" src='https://www.youtube.com/embed/videoseries?list=PLt5zc-HxINkpJSBb_oeYVsSXgpxAlxQEc&index=${getrandomNumber(numberOfEpisodes)}' frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
-        
+            videoContainer.innerHTML = `<iframe id='myFrame' width="560" height="315" src='https://www.youtube.com/embed/videoseries?list=PLt5zc-HxINkpJSBb_oeYVsSXgpxAlxQEc&index=${getrandomNumber(numberOfEpisodes)}' frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+            const iframe = document.getElementById("myFrame");
+            const videoTitle = iframe.contentWindow.document.querySelector('ytp-title-text');
+            console.log(iframe, videoTitle);
         })
     }
 })
